3abc6660213e38e0716b8394b28ed2efb051f93b
Tristan Ang <tristan-ang@hotmail.com>
2021-11-18 19:15:30 -0500

    Create github workflow

--- ghrdocker/entrypoint.sh
+++ ghrdocker/entrypoint.sh
@@ -4,7 +4,7 @@ set -eEuo pipefail
 ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 export RUNNER_ALLOW_RUNASROOT=1
 
-TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token" --header "authorization: Bearer ${GITHUB_TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
+TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token" --header "authorization: Bearer ${GH_TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
 
 /actions-runner/config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$TOKEN" --labels aks-runner
 /actions-runner/bin/runsvc.sh
